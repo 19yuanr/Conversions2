c882c7b1c15098c104c51c182d79d191804923f7
Brett Koppelman <brettkoppelman01@gmail.com>
2018-06-21 14:53:42 -0500

    Centimeters to Inches C# code

--- Conversions2/Conversions2/Controllers/FirstController.cs
+++ Conversions2/Conversions2/Controllers/FirstController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Conversions2.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,5 +16,28 @@ namespace Conversions2.Controllers
         {
             return View();
         }
+
+        public ActionResult CentToInch(){
+            CentToInchViewModel tom = new CentToInchViewModel();
+            tom.Cent = 0.0;
+            tom.Inch = 0.0;
+            return View(viewName: "CentToInch", model: tom);
+        }
+  
+        [HttpPost]
+        public ActionResult sayCentToInch(double cent, double inch){
+            CentToInchViewModel bob = new CentToInchViewModel();
+            if(cent == 0){
+                bob.Cent = inch * 2.54;
+                bob.Inch = inch;
+            }
+            if (inch == 0)
+            {
+                bob.Inch = cent / 2.54;
+                bob.Cent = cent;
+            }
+            return View(viewName: "CentToInch", model: bob);
+        }
+       
     }
 }
