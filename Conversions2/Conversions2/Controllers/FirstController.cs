d89661097d945711e88fc29df09b15d8e52ed049
Rajat Mittal <19mittar@elmbrookstudents.org>
2018-06-21 21:39:53 -0500

    CelsiusToFahren

--- Conversions2/Conversions2/Controllers/FirstController.cs
+++ Conversions2/Conversions2/Controllers/FirstController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Conversions2.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,5 +16,37 @@ namespace Conversions2.Controllers
         {
             return View();
         }
+        public ActionResult CelsiusToFahren(){
+            CelsiusToFahrenViewModel tom = new CelsiusToFahrenViewModel();
+            return View(viewName: "CelsiusToFahren", model: tom);
+        }
+
+
+        public ActionResult CelsiusToFahren2(){
+            var bob = new CelsiusToFahrenViewModel();
+            bob.Celsius = 0.0;
+            bob.Fahrenheit = 0.0;
+            return View(viewName: "CelsiusToFahren", model: bob);
+        }
+
+        [HttpPost]
+
+        public ActionResult CelsiusToFahren2(double celsius, double fahrenheit){
+            CelsiusToFahrenViewModel bob = new CelsiusToFahrenViewModel();
+            if(celsius == 0){
+                bob.Celsius = (bob.Fahrenheit-32)*0.5556;
+                bob.Fahrenheit = fahrenheit;
+            }
+            if (fahrenheit == 0)
+            {
+                bob.Fahrenheit = (bob.Celsius * 1.8) + 16;
+                bob.Celsius = celsius;
+
+            }
+            return View(viewName: "CelsiusToFahren", model: bob);
+            }
+
+        }
+        
     }
-}
+
