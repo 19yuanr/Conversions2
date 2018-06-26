81c71ee441e1ef2a19123013a73e57d29e1f09b6
Unknown <19liukat@elmbrookstudents.org>
2018-06-26 14:25:49 -0500

    new controller

---
+++ Conversions2/Conversions2/Controllers/DollarToEuroController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Conversions2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conversions2.Controllers
+{
+    public class DollarToEuroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DollarToEuro()
+        {
+            DollarToEuroViewModel init = new DollarToEuroViewModel();
+            init.Dollars = 0.0;
+            init.Euros = 0.0;
+            return View(viewName: "DollarToEuro", model: init);
+        }
+
+        [HttpPost]
+        public ActionResult ConvertDTE(double dollars, double euros)
+        {
+            DollarToEuroViewModel money = new DollarToEuroViewModel();
+            if (dollars == 0.0)
+            {
+                double newDollar = euros * 1.16;
+                money.Dollars = newDollar;
+                money.Euros = euros;
+            }
+            if (euros == 0d)
+            {
+                double newEuro = dollars * 0.86;
+                money.Dollars = dollars;
+                money.Euros = newEuro;
+            }
+            return View(viewName: "DollarToEuro", model: money);
+        }
+    }
+}
--- Conversions2/Conversions2/Controllers/FirstController.cs
+++ Conversions2/Conversions2/Controllers/FirstController.cs
@@ -17,31 +17,6 @@ namespace Conversions2.Controllers
             return View();
         }
 
-        public ActionResult DollarToEuro()
-        {
-            DollarToEuroViewModel init = new DollarToEuroViewModel();
-            init.Dollars = 0.0;
-            init.Euros = 0.0;
-            return View(viewName: "DollarToEuro", model: init);
-        }
-
-        [HttpPost]
-        public ActionResult ConvertDTE(double dollars, double euros)
-        {
-            DollarToEuroViewModel money = new DollarToEuroViewModel();
-            if(dollars == 0.0)
-            {
-                double newDollar = euros * 1.16;
-                money.Dollars = newDollar;
-                money.Euros = euros;
-            }
-            if(euros == 0d)
-            {
-                double newEuro = dollars * 0.86;
-                money.Dollars = dollars;
-                money.Euros = newEuro;
-            }
-            return View(viewName: "DollarToEuro", model: money);
-        }
+        
     }
 }
