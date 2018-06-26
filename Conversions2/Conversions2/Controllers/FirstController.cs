88f737e7e33fd5786cba3ce366767a5f907279e3
Unknown <19liukat@elmbrookstudents.org>
2018-06-26 09:27:18 -0500

    not great

--- Conversions2/Conversions2/Controllers/FirstController.cs
+++ Conversions2/Conversions2/Controllers/FirstController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Conversions2.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,6 +15,33 @@ namespace Conversions2.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult DollarToEuro()
+        {
+            DollarToEuroViewModel init = new DollarToEuroViewModel();
+            init.Dollars = 0.0;
+            init.Euros = 0.0;
+            return View(viewName: "DollarToEuro", model: init);
+        }
+
+        [HttpPost]
+        public ActionResult ConvertDTE(double dollars, double euros)
+        {
+            DollarToEuroViewModel money = new DollarToEuroViewModel();
+            if(dollars == 0.0)
+            {
+                double newDollar = euros * 1.16;
+                money.Dollars = newDollar;
+                money.Euros = euros;
+            }
+            if(euros == 0d)
+            {
+                double newEuro = dollars * 0.86;
+                money.Dollars = dollars;
+                money.Euros = newEuro;
+            }
+            return View(viewName: "DollarToEuro", model: money);
         }
     }
 }
