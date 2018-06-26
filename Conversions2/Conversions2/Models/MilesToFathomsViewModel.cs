bafab819ff350d3ea60dd523e016b1ab83ddab87
Reena Yuan <reenayuan@gmail.com>
2018-06-26 14:28:24 -0500

    lots of stuff

---
+++ Conversions2/Conversions2/Models/MilesToFathomsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Conversions2.Models
+{
+    public class MilesToFathomsViewModel
+    {
+        public double Miles { get; set; }
+        public double Fathoms { get; set; }
+
+    }
+}
